d07a8ead031315a4c35ba38f1bb51f80b80f7184
ideployonfriday <ideployonfriday@gmail.com>
2025-09-11 11:07:19 +0100

    iterate through shopping list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Banner from './components/Banner'
 import "./App.css"
 import CrocList from './components/CrocList'
+import ShoppingCart from "./components/ShoppingCart.jsx";
 
 function App() {
   return (
     <>
-      <Banner><div>Welcome to the Jungle!</div></Banner>
-      <CrocList />
+      {/*<Banner><div>Welcome to the Jungle!</div></Banner>
+      <CrocList />*/}
+        <ShoppingCart></ShoppingCart>
     </>
   );
 }
