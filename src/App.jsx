684ad8f0afb38328f44d239cd28ee87b988ed5b9
ideployonfriday <ideployonfriday@gmail.com>
2025-09-16 12:07:19 +0100

    rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
-import StarRating from "./components/StarRating.jsx";
+import {StarRadingsList} from "./components/StarRadingsList.jsx";
 
 
 function App() {
     return (
         <>
-            <section>
-                <h2>Winter's Orbit</h2>
-                <p>
-                    Rating: 5 / 5
-                </p>
-                <StarRating rating={5} />
-            </section>
-            <section>
-                <h2>Dial D for Deadman</h2>
-                <p>
-                    Rating: 4 / 5
-                </p>
-                <StarRating rating={4} />
-            </section>
-            <section>
-                <h2>Words And Things</h2>
-                <p>
-                    Rating: 0 / 5
-                </p>
-                <StarRating rating={0} />
-            </section>
-            <section>
-                <h2>The Galaxy, And The Ground Within</h2>
-                <p>
-                    Rating: 5 / 5
-                </p>
-                <StarRating rating={5} />
-            </section>
+        <StarRadingsList/>
         </>
     );
 }
