acd3f872beb3e0d3ed84acf8d95b8000fc056051
ideployonfriday <ideployonfriday@gmail.com>
2025-09-25 09:32:21 +0100

    typescript fix

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,7 +6,7 @@ export function Counter() {
     const [count, setCount] = React.useState(0);
 
 
-    function randomIntFromInterval(min, max) { // min and max included
+    function randomIntFromInterval(min: number, max: number) { // min and max included
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
