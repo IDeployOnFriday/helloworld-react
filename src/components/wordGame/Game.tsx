6d986fae426c3b845a4165e7c86b884d8ca59cbd
ideployonfriday <ideployonfriday@gmail.com>
2025-10-16 08:55:04 +0100

    win loose game

--- src/components/wordGame/Game.tsx
+++ src/components/wordGame/Game.tsx
@@ -3,21 +3,33 @@ import GuessInput from "./GuessInput.tsx";
 import {useState} from "react";
 import {sample} from "./utils.tsx";
 import {WORDS} from "./data.tsx";
+import {NUM_OF_GUESSES_ALLOWED} from "./constants.tsx";
 
 const answer = sample(WORDS)
 
     function Game(){
 
+        const [gameStatus, setGameStatus] = useState('running')
         const [guesses, setGuesses] = useState<string[]>([])
 
-        function handleSubmitGuess(guess: string) {
-            console.log("answer is", answer)
-            setGuesses(prevGuesses => [...prevGuesses, guess])
+        function handleSubmitGuess(tentativeGuess: string) {
+
+            const nextGuess =  [...guesses, tentativeGuess]
+            setGuesses(nextGuess)
+
+
+            if (tentativeGuess.toUpperCase() === answer.toUpperCase()){
+                setGameStatus('won')
+            }else if (nextGuess.length >= NUM_OF_GUESSES_ALLOWED){
+                setGameStatus('lost')
+            }
         }
 
     return (
         <>
             <GuessResults
+
+                gameStatus={gameStatus}
                 guesses={guesses}
                 answer ={answer}
             />
--- src/components/wordGame/GuessResults.tsx
+++ src/components/wordGame/GuessResults.tsx
@@ -3,14 +3,17 @@ import {range} from "./utils.tsx";
 import {NUM_OF_GUESSES_ALLOWED} from "./constants.tsx";
 
 
-function GuessResults({guesses, answer}: {guesses: string[], answer: string} ){
+function GuessResults({gameStatus, guesses, answer}: {gameStatus: string,guesses: string[], answer: string} ){
 
     return(
+        <>
+        <p>{gameStatus}</p>
         <div className="guess-results">
             {range(0, NUM_OF_GUESSES_ALLOWED).map((num) => (
                 <Guess key={num} value={guesses[num]} answer={answer} />
             ))}
         </div>
+        </>
 
     )
 }
