569b34392588fd501d0b4801e52d081c00b53ec8
ideployonfriday <ideployonfriday@gmail.com>
2025-10-17 09:40:12 +0100

    styled game

--- src/components/wordGame/Header.tsx
+++ src/components/wordGame/Header.tsx
@@ -1,10 +1,17 @@
+import {Award, Coffee} from "react-feather";
 
 function Header() {
-    return (
-        <header>
-            <h1>Word Game</h1>
-        </header>
-    );
+  return (
+    <header className="wordgame-header">
+      <div className="side" aria-hidden="true" title="Word Game">
+        <Award size={20} />
+      </div>
+      <h1 className="wordgame-title">Word Game</h1>
+      <div className="side" title="Show icons">
+          <Coffee size={20} />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
