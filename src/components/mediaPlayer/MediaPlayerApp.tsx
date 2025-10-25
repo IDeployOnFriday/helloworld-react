758fee83109d5070ebd8d7b1a8f66259c38be7d5
ideployonfriday <ideployonfriday@gmail.com>
2025-10-25 10:52:00 +0100

    adding medi player

---
+++ src/components/mediaPlayer/MediaPlayerApp.tsx
@@ -0,0 +1,14 @@
+
+import MediaPlayer from './MediaPlayer'
+
+const DEMO_SONG_SRC = 'https://storage.googleapis.com/joshwcomeau/bvrnout-take-it-easy-short.mp3';
+
+function App() {
+    return (
+        <>
+            <MediaPlayer src={DEMO_SONG_SRC} />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/mediaPlayer/VisuallyHidden.tsx
@@ -0,0 +1,30 @@
+// These styles will make sure the component
+// is not visible, but will still be announced
+// by screen readers.
+//
+// Adding “display: none” would hide the
+// element from ALL users, including those
+// using screen-readers.
+import {CSSProperties, ReactNode} from "react";
+
+const hiddenStyles = {
+    display: 'inline-block',
+    position: 'absolute',
+    overflow: 'hidden',
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    width: 1,
+    margin: -1,
+    padding: 0,
+    border: 0,
+}  as CSSProperties;
+
+const VisuallyHidden = ({ children }: {children : ReactNode} ) => {
+    return (
+        <span style={hiddenStyles}>
+      {children}
+    </span>
+    );
+};
+
+export default VisuallyHidden;
