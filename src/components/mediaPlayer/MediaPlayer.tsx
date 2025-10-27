40b982ba3d5451a4b1289c8e964459e224874228
ideployonfriday <ideployonfriday@gmail.com>
2025-10-27 08:44:03 +0000

    playing media

--- src/components/mediaPlayer/MediaPlayer.tsx
+++ src/components/mediaPlayer/MediaPlayer.tsx
@@ -1,8 +1,11 @@
 import VisuallyHidden from './VisuallyHidden';
 import { Play } from 'react-feather';
+import React from "react";
 
 function MediaPlayer({ src }: { src: string }) {
-  return (
+    const audioRef = React.useRef<HTMLAudioElement | null>(null);
+
+    return (
     <div className="wrapper">
       <div className="media-player">
         <img alt="" src="https://sandpack-bundler.vercel.app/img/take-it-easy.png" />
@@ -10,12 +13,20 @@ function MediaPlayer({ src }: { src: string }) {
           <h2>Take It Easy</h2>
           <p>Bvrnout ft. Mia Vaile</p>
         </div>
-        <button>
+        <button
+            onClick={() => {
+                console.log("audioRef.current", audioRef.current)
+                if  (audioRef.current) {
+                    console.log("start song ")
+                    audioRef.current.play();
+                }
+            }}
+        >
           <Play />
           <VisuallyHidden>Toggle playing</VisuallyHidden>
         </button>
 
-        <audio src={src} />
+          <audio ref={audioRef} src={src} />
       </div>
     </div>
   );
