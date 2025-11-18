3213b493abb365d65f74a1f18b38483b967d99ff
ideployonfriday <ideployonfriday@gmail.com>
2025-11-18 09:40:48 +0000

    removing unsused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import ToastyApp from "./components/toasty/ToastyApp.tsx";
-import WindowSize from "./components/resizeWindow/WindowSize.js";
-import ColorPicker from "./components/ColorPicker.js";
-import MyColorPicker from "./components/MyColorPicker.js";
-import AnotherColorPicker from "./components/colorPickers/AnotherColorPicker.js";
 import VideoApp from "./components/videoPlayer/VideoApp.js";
 import Header from "./components/Header.js";
 
