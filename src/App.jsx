878ddc8c4b0788c6e34dcd69b73f30fd3f83ac9d
ideployonfriday <ideployonfriday@gmail.com>
2025-11-25 08:27:05 +0000

    two buttons for reorder

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import VideoApp from "./components/videoPlayer/VideoApp.js";
 import Header from "./components/Header.js";
 import DropdownMenu from "./components/dropdownMenu/DropdownMenu";
 import ToastyApp from "./components/toasty/ToastyApp.js";
+import {OrderingPage} from "./components/reorderingList/OrderingPage.js";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
             <div className="wrapper" style={{backgroundColor: 'lightblue', padding: '20px'}}>
 
 
-            <ToastyApp></ToastyApp>
+            <OrderingPage></OrderingPage>
             </div>
         </>
 
---
+++ src/components/reorderingList/OrderingPage.tsx
@@ -0,0 +1,15 @@
+
+
+export function OrderingPage() {
+
+    return (
+<>
+        <button onClick={() => console.log('move up')}>
+            move up
+        </button>
+        <button onClick={() => console.log('move up')}>
+            move down
+        </button>
+</>
+    )
+}
